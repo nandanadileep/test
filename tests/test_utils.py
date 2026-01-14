e15a153d213fb7935ccf411faec2c9dd22b925c2
Nandana Dileep <nandana@Nandanas-MacBook-Air.local>
2026-01-14 20:09:37 +0530

    edited test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,6 @@ from src.utils import add
 def test_add():
     assert add(1, 2) == 192  
 def test_add_negative():
-    assert add(-1, -1) == 100  
+    assert add(-1, -1) == 101
 def test_add_zero():
     assert add(5, 0) == 5
