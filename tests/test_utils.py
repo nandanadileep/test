3f92c50ba3b8cbb00946b8f078dc029a3742c9b7
Nandana Dileep <nandana@Nandanas-MacBook-Air.local>
2026-01-15 01:05:12 +0530

    test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from src.utils import add
 
 def test_add():
-    assert add(987, 0) == 100
+    assert add(987, 0) == 101
 def test_add_negative():
     assert add(-1, -1) == -2
 def test_add_zero():
