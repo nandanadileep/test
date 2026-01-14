51058d9b933f589dccaafbc6a653fd5a3d85be1e
Nandana Dileep <110280757+nandanadileep@users.noreply.github.com>
2026-01-14 19:55:28 +0530

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,8 @@
 from src.utils import add
 
 def test_add():
-    # ❌ BREAKING: Wrong expected value
-    assert add(1, 2) == 5   # Should be 3, but we wrote 5
-    
+    assert add(1, 2) == 192  
 def test_add_negative():
-    # ❌ BREAKING: Wrong expected value  
-    assert add(-1, -1) == 0  # Should be -2, but we wrote 0
-
+    assert add(-1, -1) == 100  
 def test_add_zero():
-    # ✅ This one is correct
     assert add(5, 0) == 5
